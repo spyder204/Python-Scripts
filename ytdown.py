dd10424e6aa51d3b1f4acb2922391f8b24405880
Vibhor Tiwari <47317582+spyder204@users.noreply.github.com>
2019-10-13 18:03:33 +0530

    Update ytdown.py

--- ytdown.py
+++ ytdown.py
@@ -4,9 +4,6 @@ from selenium.webdriver.common.keys import Keys
 
 import os,time
 from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("--disable-notifications")#doesn't seem to work but not an error either
 driver=webdriver.Chrome()
 file=open("links.txt","r")
 
@@ -16,11 +13,13 @@ for i in file:
 	#print(i[0:])
 	driver.get(i[0:])
 	options.add_argument("--disable-notifications")
-	time.sleep(8)#lower this depending on your internet speed
+	time.sleep(8)#lower if your internet's fast
 
 	driver.find_element_by_xpath("/html/body/div/div[1]/div[3]/div[4]/div/div[1]/div[2]/div[2]/div[1]/a").click()
 	time.sleep(2)
-	driver.switch_to.window(driver.window_handles[0])
+	driver.switch_to.window(driver.window_handles[0])//
+	"" savefrom.net website redirects you to a new tab. I haven't
+	figured out a way to close it, as of yet, so line 20 just takes you back to the original tab***
 	
 
 	
